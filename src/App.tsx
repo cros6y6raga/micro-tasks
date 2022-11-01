974890bd3f960eecffab997d42b9d2e15f7acc58
cros6y6raga <crosbybraga@gmail.com>
2022-11-01 15:49:18 +0300

    Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, MouseEvent} from 'react';
 import './App.css';
 import {NewComponent} from "./tasks/map/NewComponent";
+import {Button} from "./tasks/button/Button";
 
 function App() {
     const [students, setStudents] = useState([
@@ -22,8 +23,47 @@ function App() {
         {manufacturer: 'Mercedes', model: 'e63s'},
         {manufacturer: 'Audi', model: 'rs6'}
     ]
+    // const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+    //     console.log('Hello Im Vasya')
+    // }
+    // const mySecondSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+    //     console.log('Hello Im Ivan')
+    // }
+
+    // const onClickHandler=(name:string)=>{
+    //     console.log(name)
+    // }
+
+    // const foo1 = (event: MouseEvent<HTMLButtonElement>) => {
+    //     console.log(100200)
+    // }
+    // const foo2 = (num: number) => {
+    //     console.log(num)
+    // }
+    const Button1Foo = (sub: string, age: number) => {
+        console.log(sub, age)
+    }
+    const Button2Foo = (sub: string) => {
+        console.log(sub)
+    }
+    const Button3Foo = (sub: string) => {
+        console.log(sub)
+    }
     return (
-        <NewComponent students={students} topCars={topCars}/>
+        <div className='App'>
+            <NewComponent students={students} topCars={topCars}/>
+            {/*<button onClick={(event) => {console.log('Hello')}}>MyYouTubeChannel-1</button>*/}
+            {/*<button onClick={(event)=>onClickHandler('I\'m Vasya')}>MyYouTubeChannel-1</button>*/}
+            {/*<button onClick={(event)=>onClickHandler('I\'m Ivan')}>MyYouTubeChannel-2</button>*/}
+            {/*<button onClick={()=>onClickHandler('some info')}>MyYouTubeChannel-3</button>*/}
+            {/*<button onClick={foo1}>1</button>*/}
+            {/*<button onClick={(event) => foo2(100200)}>2</button>*/}
+            {/*<button>MyYouTubeChannel-1</button>*/}
+            {/*<button>MyYouTubeChannel-2</button>*/}
+            <Button name={'MyYouTubeChannel-1'} callBack={() => Button1Foo('I\' am Vasya', 21)}/>
+            <Button name={'MyYouTubeChannel-2'} callBack={() => Button2Foo('I\' am Ivan')}/>
+            <Button name={'MyYouTubeChannel-3'} callBack={() => Button3Foo('I\' am stupid button')}/>
+        </div>
     );
 }
 
